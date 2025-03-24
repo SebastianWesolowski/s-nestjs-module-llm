4e16868f1c6fc5c48ce125f7c0aee94890f1dac7
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2025-03-24 15:02:48 +0100

    fix: 🐛 gh token
    Signed-off-by: Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>

--- dist/main.js
+++ dist/main.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const swagger_1 = require("@nestjs/swagger");
-const my_module_module_1 = require("./my-module.module");
+const my_module_module_1 = require("@/my-module.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(my_module_module_1.MyModule);
     const config = new swagger_1.DocumentBuilder()
         .setTitle('S-Nest Module LLM')
         .setDescription('API dokumentacja dla S-Nest Module LLM')
         .setVersion('1.0')
-        .addTag('s-nest-module-llm')
+        .addTag('s-nestjs-module-llm')
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('docs', app, document);
